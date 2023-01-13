3f55877d9af430b15059b6a7d298b61d0067f639
Gardene10 <gardene.v.c@gmail.com>
2023-01-13 09:02:17 -0300

    refactor: move HttpClientSpy to its own file°ˀ→

--- src/domain/data/usecases/authentication/remote-authentication.spec.ts
+++ src/domain/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,16 +1,28 @@
-/* eslint-disable no-trailing-spaces */
 /* eslint-disable @typescript-eslint/indent */
 
-
+import { url } from 'inspector'
+import { type } from 'os'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuthentication } from './remore-authentication'
 
+type SutTypes = {
+ sut: RemoteAuthentication
+ httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    return {
+        sut,
+        httpPostClientSpy
+    }
+}
 
 describe('RemoteAuthentication', () => {
     test('should call Http with correct URL', async () => {
-        const url = 'any_url'
-        const httpPostClientSpy = new HttpPostClientSpy()
-        const sut = new RemoteAuthentication(url, httpPostClientSpy)
+        const url = 'other_url'
+        const { sut, httpPostClientSpy } = makeSut(url)
         await sut.auth()
         expect(httpPostClientSpy.url).toBe(url)
     })
