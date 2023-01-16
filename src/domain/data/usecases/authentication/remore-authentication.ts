b7e1b9882902855a39d2cfeaf80e7400cd56644d
Gardene10 <gardene.v.c@gmail.com>
2023-01-16 20:24:45 -0300

    test: ensure Remote Authentication returns an account on success

--- src/domain/data/usecases/authentication/remore-authentication.ts
+++ src/domain/data/usecases/authentication/remore-authentication.ts
@@ -1,25 +1,26 @@
 
 import { InvalidCredentialsError } from '../../../errors/invalid-credentials-error'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpPostClient } from '../../protocols/http/http-post-client'
 import { HttpStausCode } from '../../protocols/http/http-response'
 import { UnexpectedError } from '../../../errors/unexpected-error'
 import { AccountModel } from '@/domain/models/account-model'
 
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
     switch (httpResponse.statusCode){
-      case HttpStausCode.ok: break
+      case HttpStausCode.ok: return httpResponse.body
+      
       case HttpStausCode.unathorazed: throw new InvalidCredentialsError()
       default: throw new UnexpectedError()
     }
