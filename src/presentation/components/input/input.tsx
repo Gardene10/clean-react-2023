5f30c77b3ff9b03a3cd3f29b0f0f03fa8a9c4b46
Gardene10 <gardene.v.c@gmail.com>
2023-01-26 18:55:45 -0300

    refactor: move form to a separete file

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,15 +1,14 @@
 
-import { type } from "os"
+
 import React from "react"
-import Styles from './input-styles.scss'
 
-type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-const Footer: React.FC<Props> = (props: Props) => {
+
+const Input: React.FC= () => {
     return (
-        <input {...props} />
+        <input />
 
     )
 }
 
-export default Footer
+export default Input
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,36 +1,29 @@
 import React from "react";
 import Styles from './login-styles.scss'
-import Spinner from "@/presentation/components/spinner/spinner";
 import Header from "@/presentation/components/login-header/login-header";
 import Footer from "@/presentation/components/footer/footer";
-import Input from "@/presentation/components/inputr/input";
+import Input from "@/presentation/components/form-status/form-status";
+import FormStatus from "@/presentation/components/input/input";
+
 const Login: React.FC =() => {
     return (
         <div className={Styles.login}>
             <Header/>
-            
-            
-           
-            
 
             <form className={Styles.form}>
                 <h2>Login</h2>
+                <Input/>
                 
-                <Input type="email" name="email" placeholder="Digite seu e-mail" />
-                <Input type="password" name="password" placeholder="Digite sua senha" />
+                <input type="email" name="email" placeholder="Digite seu e-mail" />
+                <input type="password" name="password" placeholder="Digite sua senha" />
 
                 <button className={Styles.submit} type="submit">Entrar</button>
                 <span className={Styles.link}>Criar conta</span>
-
-                <div className={Styles.errorWrap}>
-                    <Spinner className={Styles.spinner} />
-                    
-                    <span className={Styles.error}>Erro</span>
-                </div>
+                <FormStatus/>
             </form>
             <Footer/>
           </div>
     )
 }
 
-export default Login
+export default Login 
