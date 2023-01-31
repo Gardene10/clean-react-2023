3fa61df514ce2fb1c8c2ca547cfe11b6d690d32c
Gardene10 <gardene.v.c@gmail.com>
2023-01-30 21:29:28 -0300

    feat: ensure Login will not load spinner and error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,16 +1,18 @@
 
-import React from "react"
+import React from 'react'
 import Styles from './form-status-styles.scss'
 import Spinner from "@/presentation/components/spinner/spinner";
-
+import Context from '@/presentation/contexts/form/form-context'
+import { useContext } from 'react'
 
 
 const FormStatus: React.FC = () => {
+    const { isLoading, errorMessage  } = useContext(Context)
     return (
-        <div className={Styles.errorWrap}>
-        <Spinner className={Styles.spinner} />
+        <div data-testid="error-wrap" className={Styles.errorWrap}>
+        { isLoading && <Spinner className={Styles.spinner} />}
         
-        <span className={Styles.error}>Erro</span>
+        {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
     </div>
 
     )
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import Styles from './login-styles.scss'
 import { LoginHeader, FormStatus,Input,Footer} from "@/presentation/components";
+import Context from '@/presentation/contexts/form/form-context'
+
+type stateProps = {
+    isLoading: boolean
+    errorMessage: string
+
+}
 
 const Login: React.FC =() => {
+   const [state] = useState<stateProps>({
+    isLoading: false,
+    errorMessage: ''
+
+   })
+
     return (
         <div className={Styles.login}>
             <LoginHeader/>
 
+            <Context.Provider value={state}>
+
             <form className={Styles.form}>
                 <h2>Login</h2>
 
-                
-                
                 <input type="email" name="email" placeholder="Digite seu e-mail" />
                 <input type="password" name="password" placeholder="Digite sua senha" />
 
@@ -19,6 +32,9 @@ const Login: React.FC =() => {
                 <span className={Styles.link}>Criar conta</span>
                 <FormStatus/>
             </form>
+
+            </Context.Provider>
+
             <Footer/>
           </div>
     )
