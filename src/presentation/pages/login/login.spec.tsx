52a5c464f6a0e3a5ce579e6e00711661e7ee337e
Gardene10 <gardene.v.c@gmail.com>
2023-02-06 09:53:16 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,22 +5,22 @@ import { Validation } from '@/presentation/protocols/validation'
 
 type SutTypes = {
   sut: RenderResult
-  validationSpy: validationSpy
+  validationSpy: ValidationSpy
 }
 
-class validationSpy implements Validation {
+class ValidationSpy implements Validation {
   errorMessage: string
   input: object
 
   validate (input: object): string{
-    this.input = input
+   this.input = input
     return this.errorMessage
   }
 
 }
 
 const makeSut = (): SutTypes => {
-  const validationSpy = new validationSpy()
+  const validationSpy = new ValidationSpy()
   const sut = render(<Login validation={validationSpy}/>)
   return{
     sut,
@@ -33,22 +33,30 @@ describe('Login Component',()=> {
 
     test('Should start with initial state',() => {
       const {sut } = makeSut()
-  
-      const subimitButton = sut.getByTestId ('submit') as HTMLButtonElement
-      expect(subimitButton.disabled).toBe(true)
+      
+      const errorWrap = sut.getByTestId('error-wrap')
+      expect(errorWrap.childElementCount).toBe(0)
+
+      const submitButton = sut.getByTestId ('submit') as HTMLButtonElement
+      expect(submitButton.disabled).toBe(true)
+
+      const emailStatus = sut.getByTestId('email-status')
+      expect(emailStatus.title).toBe('campo obrigatorio')
+      expect(emailStatus.textContent).toBe('🔴')
+
+      const passwordStatus = sut.getByTestId('password-status')
+      expect(passwordStatus.title).toBe('campo obrigatorio')
+      expect(passwordStatus.textContent).toBe('🔴')
     })
 
-    test('Should call Validation with correct value',() => {
+    test('Should call Validation with correct email',() => {
       const {sut, validationSpy } = makeSut()
       const emailInput = sut.getByTestId('email')
       fireEvent.input(emailInput,{target: {value: 'any_email'}})
       expect(validationSpy.input).toEqual({
         email: 'any_email'
       })
-
-  
   
     })
-
     
 })
