3b9b7a26d230652faba61ef6721c68b1dd35fe33
Gardene10 <gardene.v.c@gmail.com>
2023-02-07 14:22:36 -0300

    feat: ensure Login present error if email is valid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -15,12 +15,16 @@ const Login: React.FC<Props>=({validation}: Props) => {
     password:'',
     isLoading: false,
     mainError:'',
-    emailError:'campo obrigatorio',
-    passwordError:'campo obrigatorio'
+    emailError:'',
+    passwordError:''
 
    })
    useEffect(()=> {
-    validation.validate('email', state.email)
+    setState({
+        ...state,
+        emailError:validation.validate('email', state.email)
+    })
+    
    },[state.email])
 
    useEffect(()=> {
