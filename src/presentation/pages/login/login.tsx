a68ce43075f42413ad474d0145a26a5a35b8c4ea
Gardene10 <gardene.v.c@gmail.com>
2023-02-15 21:37:30 -0300

    feat: ensure Login navegates to main page on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,5 +1,5 @@
 import React, {useState,useEffect} from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory} from "react-router-dom";
 import Styles from './login-styles.scss'
 import {LoginHeader, FormStatus,Input,Footer, Spinner} from "@/presentation/components";
 import Context from '@/presentation/contexts/form/form-context'
@@ -15,6 +15,7 @@ type Props = {
 }
 
 const Login: React.FC<Props>=({validation,authentication}: Props) => { 
+   const history = useHistory()
    const [state,setState] = useState({
     email:'',
     password:'',
@@ -46,6 +47,7 @@ const Login: React.FC<Props>=({validation,authentication}: Props) => {
           password: state.password
        })
         localStorage.setItem('accessToken', account.accessToken)
+        history.replace('/')
     } catch (error) {
         setState({ 
             ...state ,
