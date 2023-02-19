7340b26f3fff84fb784dfd96b7df077cf4982dff
Gardene10 <gardene.v.c@gmail.com>
2023-02-19 17:09:16 -0300

    refactor: move required field validation to a different folder

--- src/validation/validators/required-field/required-field-validation.spec.ts
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -1,5 +1,5 @@
 import { RequiredFieldError } from "@/validation/errors"
-import { RequiredFieldValidation } from "@/validation/required-field-validation"
+import { RequiredFieldValidation } from "./required-field-validation"
 import faker from 'faker'
 
 const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
