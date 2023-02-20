733a8314c8a3d4e310007ac84f222ab53acf4f9a
Gardene10 <gardene.v.c@gmail.com>
2023-02-20 15:20:55 -0300

    test: ensure ValidationBuilder has a email helper

--- src/validation/validators/builder/validation-builder.ts
+++ src/validation/validators/builder/validation-builder.ts
@@ -1,5 +1,5 @@
 import { FieldValidation } from "@/validation/protocols/field-validation"
-import { RequiredFieldValidation } from "@/validation/validators"
+import { RequiredFieldValidation, EmailValidation } from "@/validation/validators"
 
 export class ValidationBuilder {
     private constructor(
@@ -15,6 +15,13 @@ export class ValidationBuilder {
         return this
 
     }
+
+    email(): ValidationBuilder{
+        this.validations.push(new EmailValidation(this.fieldName))
+        return this
+
+    }
+
     build (): FieldValidation[] {
         return this.validations
 
