34a9a90d47b8e6294c40f7c158709f3e0cd2ddef
Gardene10 <gardene.v.c@gmail.com>
2023-02-20 15:21:41 -0300

    test: ensure ValidationBuilder has a email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,5 +1,5 @@
 
-import { RequiredFieldValidation } from "@/validation/validators"
+import { RequiredFieldValidation, EmailValidation } from "@/validation/validators"
 import { ValidationBuilder as sut } from "./validation-builder"
 
 
@@ -7,6 +7,12 @@ describe('ValidationBuilder',() => {
     test('Shold return RequiredFieldValidation',() =>{
         const validations = sut.field('any_field').required().build()
         expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+
+    })
+
+    test('Shold return EmailValidation',() =>{
+        const validations = sut.field('any_field').email().build()
+        expect(validations).toEqual([new EmailValidation('any_field')])
 
     })
 })
