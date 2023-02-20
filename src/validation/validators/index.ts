a0de0934738281d1885fa79b830bddcac1554043
Gardene10 <gardene.v.c@gmail.com>
2023-02-20 14:59:02 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,4 @@
+export * from './required-field/email/email-validation'
+export * from './min-length/min-length-validation'
+export * from './required-field/required-field-validation'
+export * from './validation-composite/validation-composite'
