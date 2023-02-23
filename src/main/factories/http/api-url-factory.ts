13333346669a3e8a28a54af54638076dda2ca727
Gardene10 <gardene.v.c@gmail.com>
2023-02-23 16:02:15 -0300

    feat: add env var to API URL

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,6 +1,6 @@
 
 export const makeApiUrl = (path: string): string => {
-    return `http://fordevs.herokuapp.com/api${path}`
+    return `process.env.API_URL${path}`  //definição de variaveis de ambiente //esta no webpack
 
 
 }
