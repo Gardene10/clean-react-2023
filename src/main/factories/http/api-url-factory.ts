da10ab0ab201df2887bb9ea55c515c9e3b83cd59
Gardene10 <gardene.v.c@gmail.com>
2023-02-24 22:48:35 -0300

    fix: change webpack env var to use JSON.stringfy

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,6 +1,6 @@
 
 export const makeApiUrl = (path: string): string => {
-    return `process.env.API_URL${path}`  //definição de variaveis de ambiente //esta no webpack
+    return `${process.env.API_URL}${path}`  //definição de variaveis de ambiente //esta no webpack
 
 
 }
