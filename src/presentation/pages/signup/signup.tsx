8625b4fc66118663d3584f6f300c5dc1f4f607e1
Gardene10 <gardene.v.c@gmail.com>
2023-03-06 17:21:55 -0300

    feat: ensure SignUp change email  status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -15,8 +15,9 @@ const SignUp: React.FC<Props> =({validation}: Props) => {
 
         isLoading: false,
         name:'',
-        nameError:'Campo Obrigatorio',
-        emailError:'Campo Obrigatorio',
+        email:'',
+        nameError:'',
+        emailError:'',
         passwordError:'Campo Obrigatorio',
         passwordConfirmationError:'Campo Obrigatorio',
         mainlError:'',
@@ -26,10 +27,11 @@ const SignUp: React.FC<Props> =({validation}: Props) => {
        useEffect(()=> {
         setState({
             ...state,
-            nameError: validation.validate('name', state.name)
+            nameError: validation.validate('name', state.name),
+            emailError: validation.validate('email', state.email)
         })
         
-       },[state.name])
+       },[state.name, state.email])
         
 
 
