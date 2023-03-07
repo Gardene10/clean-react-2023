57ea2149d50a38eab8b556ae21d9330b3be1006f
Gardene10 <gardene.v.c@gmail.com>
2023-03-07 11:13:28 -0300

    feat: ensure SignUp calls AddAccount with correct values

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -42,6 +42,7 @@ const SignUp: React.FC<Props> =({validation, addAccount}: Props) => {
        const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
         event.preventDefault()
         setState({...state, isLoading: true})
+        
         await addAccount.add({
             name: state.name,
             email: state.email,
