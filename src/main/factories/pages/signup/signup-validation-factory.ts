2e499998291a706a7815f2d277e31cfbfcb7bf7c
Gardene10 <gardene.v.c@gmail.com>
2023-03-09 18:36:03 -0300

    feat: add SignUp composition

--- src/main/factories/pages/signup/signup-validation-factory.ts
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -9,7 +9,7 @@ export const makeSignUpValidation = (): ValidationComposite => {
        ...Builder.field('name').required().min(5).build(),
        ...Builder.field('email').required().email().build(),
        ...Builder.field('password').required().min(5).build(),
-       ...Builder.field('passwordConfirmation').required().min(5).build()
+       ...Builder.field('passwordConfirmation').sameAs('password').build()
     ])
   
 
